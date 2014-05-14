c21447ce5d3fff343cf0c424ff6aeb83b401e78b
László Balla-Kovács <balla.kovacs.laszlo@gmail.com>
2014-05-14 09:00:30 +0200

    Update pdf-rotate.py

--- pdf-rotate.py
+++ pdf-rotate.py
@@ -1,8 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+# open "pdf" directory and all subdirectories
+# all file rotate Clockwise 90 degrees and save same name
+
+import sys
+import os
 import magic
 
-# check open file, is pdf?
+from PyPDF2 import PdfFileWriter, PdfFileReader
 
-file_type = magic.from_file("issue30_hu.pdf", mime=True)
-if file_type == b'application/pdf':
-    print('Yes')
+for dirpaths, dirnames, filenames in os.walk("pdf"):
+    for fname in filenames:
+        fnamepath = dirpaths + "/" + fname
+        file_type = magic.from_file(fnamepath, mime=True)
+        
+        if file_type == b'application/pdf':
+            input = PdfFileReader(fnamepath, "rb")
+            output = PdfFileWriter()
+
+            for i in range(0,input.getNumPages()):
+                output.addPage(input.getPage(i).rotateClockwise(90))
+
+            outputStream = open(fnamepath, "wb")
+            output.write(outputStream) 
+
+
