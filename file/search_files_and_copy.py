8c63f71661fbc34d751a5c9cc0bbb25441ccb089
László Balla-Kovács <balla.kovacs.laszlo@gmail.com>
2018-01-24 15:33:45 +0100

    adding utf-8

--- file/search_files_and_copy.py
+++ file/search_files_and_copy.py
@@ -6,8 +6,7 @@ import os, shutil, csv
 srcroot = input('source folder: ').strip()
 dstroot = input('destination folder: ').strip()
 
-#extensions = ['doc', 'docx', 'xls', 'xlsx', 'pdf', 'odf', 'rtf', 'txt', 'csv']
-extensions = ['xls', 'xlsx']
+extensions = ['doc', 'docx', 'xls', 'xlsx', 'pdf', 'odf', 'rtf', 'txt', 'csv']
 
 def walk_and_copy(srcroot, dstroot, extensions):
     ''' walk on srcroot, and create same dirs in dstroot, and copy files in extensions '''
@@ -23,15 +22,17 @@ def walk_and_copy(srcroot, dstroot, extensions):
             os.makedirs(new_path)
         
         # create csv file
-        with open(dstroot + '/!file_list.csv', 'a', newline='', encoding = 'iso-8859-2') as csvfile:
+        with open(dstroot + '/!file_list.csv', 'a', newline='', encoding = 'utf-8') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=';')
             
             # iter all filenames in path, and copy this to the dstroot, when in extensions
             for filename in files:        
                 if filename.lower().split('.')[1] in extensions:
                     shutil.copy2(path + '/' + filename, new_path)                
-                    spamwriter.writerow([path.replace(srcroot, ''), filename])
+                    try:
+                        spamwriter.writerow([path.replace(srcroot, ''), filename])
+                    except UnicodeEncodeError:
+                        print(path, filename)
 
 if __name__ == '__main__':
     walk_and_copy(srcroot, dstroot, extensions)
-    
